2e9290a7c3c5e8d3618a5279a94052baac6a6f6a
SpecsaversEaton <kallen.williams@specsavers.com>
2026-05-28 16:21:29 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ClinicCoordinator from './ClinicCoordinator.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ClinicCoordinator from "./ClinicCoordinator.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClinicCoordinator />
   </React.StrictMode>
-)
+);
